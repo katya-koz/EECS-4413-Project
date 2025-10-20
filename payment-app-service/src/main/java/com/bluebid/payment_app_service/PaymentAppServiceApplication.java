f362be11c222879294756538e535e8ee78b48745
ajarm <ajarm@10.0.0.20>
2025-10-20 19:48:47 -0400

    Added payment validation (luhns algorithm, simple junit test cases)

--- payment-app-service/src/main/java/com/bluebid/payment_app_service/PaymentAppServiceApplication.java
+++ payment-app-service/src/main/java/com/bluebid/payment_app_service/PaymentAppServiceApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PaymentAppServiceApplication {
+public class PaymentAppServiceApplication 
+{
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentAppServiceApplication.class, args);
+		
 	}
+	
+	
 
 }
--- payment-app-service/src/test/java/com/bluebid/payment_app_service/PaymentAppServiceApplicationTests.java
+++ payment-app-service/src/test/java/com/bluebid/payment_app_service/PaymentAppServiceApplicationTests.java
@@ -1,13 +1,71 @@
 package com.bluebid.payment_app_service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PaymentAppServiceApplicationTests {
+class PaymentAppServiceApplicationTests 
+{
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void validateCard()
+	{
+		String cardNumber = "4506445393645302";
+		int expiryMonth = 01;
+		int expiryYear = 2026;
+		String cvv = "123";
+		
+		PaymentValidation validate = new PaymentValidation(cardNumber, expiryMonth, expiryYear, cvv);
+		
+		assertEquals(true, validate.validateCard());
+		
+	}
+	
+	@Test
+	void checkWrongExpiryMonth()
+	{
+		String cardNumber = "4506445393645302";
+		int expiryMonth = 13;
+		int expiryYear = 2026;
+		String cvv = "123";
+		
+		PaymentValidation validate = new PaymentValidation(cardNumber, expiryMonth, expiryYear, cvv);
+		
+		assertEquals(false, validate.validateCard());
+		
+	}
+	@Test
+	void checkWrongExpiryYear()
+	{
+		String cardNumber = "4506445393645302";
+		int expiryMonth = 01;
+		int expiryYear = 2025;
+		String cvv = "123";
+		
+		PaymentValidation validate = new PaymentValidation(cardNumber, expiryMonth, expiryYear, cvv);
+		
+		assertEquals(false, validate.validateCard());
+		
+	}
+
+	@Test
+	void checkWrongCVV()
+	{
+		String cardNumber = "4506445393645302";
+		int expiryMonth = 01;
+		int expiryYear = 2025;
+		String cvv = "45678";
+		
+		PaymentValidation validate = new PaymentValidation(cardNumber, expiryMonth, expiryYear, cvv);
+		
+		assertEquals(false, validate.validateCard());
+		
 	}
 
 }
