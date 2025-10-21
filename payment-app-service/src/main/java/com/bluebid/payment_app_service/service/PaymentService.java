6b2a33bfd54233c7235bf5c0c22ed59b14c35261
katya-koz <katya.kozlovsky@gmail.com>
2025-10-20 20:30:59 -0400

    fixed merge

--- payment-app-service/src/main/java/com/bluebid/payment_app_service/service/PaymentService.java
+++ payment-app-service/src/main/java/com/bluebid/payment_app_service/service/PaymentService.java
@@ -1,29 +1,131 @@
 package com.bluebid.payment_app_service.service;
 
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
 	
-	 public boolean isValidCreditCard(String cardNumber) {
-		 // luhns algo
-	        if (cardNumber == null || !cardNumber.matches("\\d+")) { // card should be digits only
-	            return false; 
-	        }
+	
+	public boolean isValidCard(String cardNumber, String expiryMonth, String expiryYear, String cvv)
+	{
+		if (validateCardNumber(cardNumber) && validateExpirationDate(expiryMonth, expiryYear) && validateCvv(cvv))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	private boolean validateCardNumber(String cardNumber)
+	{
+		boolean isCardValid;
+		
+		//reverse the string
+		String cardNumberReversed = new StringBuilder(cardNumber).reverse().toString();
+		
 
-	        int sum = 0;
-	        boolean alternate = false;
-	        for (int i = cardNumber.length() - 1; i >= 0; i--) {
-	            int n = Integer.parseInt(cardNumber.substring(i, i + 1));
-	            if (alternate) {
-	                n *= 2;
-	                if (n > 9) {
-	                    n -= 9;
-	                }
-	            }
-	            sum += n;
-	            alternate = !alternate;
-	        }
-	        return (sum % 10 == 0);
-	    }
+		//implement luhn's alogirthm
+		int luhns = luhnsAlgorithm(cardNumberReversed);
+		
+		if (luhns % 10 == 0)
+		{
+			isCardValid = true;
+		}
+		else
+		{
+			isCardValid = false;
+			
+		}
+		
+		
+		return isCardValid;
+	}
+	
+	private boolean validateExpirationDate(String expiryMonth, String expiryYear)
+	{
+		
+		int expiryMonthInt = Integer.parseInt(expiryMonth);
+		int expiryYearInt = Integer.parseInt(expiryYear);
+		
+		int currentMonth = LocalDate.now().getMonthValue();
+		int currentYear = LocalDate.now().getYear();
+		
+		if (expiryMonthInt < 1 || expiryMonthInt > 12 || expiryYearInt < currentYear) //checking whether they have entered a valid month
+		{
+			return false;
+		}
+		
+		
+		if ((expiryMonthInt < currentMonth) && (expiryYearInt == currentYear))
+		{
+			return false;
+		}
+		
+		return true;
+	}
+	
+	private boolean validateCvv(String cvv)
+	{
+		boolean validCV;
+		if (cvv == null)
+		{
+			return false;
+		}
+		cvv = cvv.trim();
+		validCV = cvv.matches("\\d{3,4}");
+	
+		//checking the number is at least 3 and at most 4 digits
+		return validCV;
+	}
+	
+	//integer for doubling the value
+	private int doubleVal(char value)
+	{
+		int result = 0;
+		
+		result = Character.getNumericValue(value);
+		result *= 2;
+		
+		return result;
+	}
+	
+	private int luhnsAlgorithm(String s)
+	{
+		int total = 0;
+		int temp = 0;
+		
+		for (int i = 0; i < s.length(); i++)
+		{
+			if ((i%2) == 1)//check whether the index is odd
+			{
+				char c = s.charAt(i);
+				temp = doubleVal(c); //double the value
+				if (temp > 9)
+				{
+					int digit = 0;
+					while (temp > 0) //getting the value of the two digits added
+					{
+						digit += temp % 10;
+						temp = temp / 10;
+					}
+					
+					temp = digit;
+					
+				}
+				
+				total += temp;
+
+			}else {
+				//even, just add
+				total += Character.getNumericValue(s.charAt(i));
+			}
+			
+			
+		}
+		return total;
+	}
 }
