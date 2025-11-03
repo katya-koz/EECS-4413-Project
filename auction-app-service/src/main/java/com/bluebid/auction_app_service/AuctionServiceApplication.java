d7099ae951892ef5556223304f9459f05ea648d6
katya-koz <katya.kozlovsky@gmail.com>
2025-11-02 23:47:06 -0500

    automatic auction closer

--- auction-app-service/src/main/java/com/bluebid/auction_app_service/AuctionServiceApplication.java
+++ auction-app-service/src/main/java/com/bluebid/auction_app_service/AuctionServiceApplication.java
@@ -2,7 +2,9 @@ package com.bluebid.auction_app_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class AuctionServiceApplication {
 
